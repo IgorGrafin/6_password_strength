c4d0532eff061f0db8892e70844a212a88d82f34
Igor Grafin <apaticmail@gmail.com>
2018-01-30 00:58:44 +0300

    added get_blacklisted_words and getpass

--- password_strength.py
+++ password_strength.py
@@ -1,9 +1,10 @@
 import re
+import os
+import getpass
 
 
 def has_upper_and_lower_case(password):
-    count = 0
-    if re.match(".*[a-z]+", password) and re.match(".*[A-Z]+", password):
+    if re.search("[a-z]", password) and re.search("[A-Z]", password):
         return 2
     return -2
 
@@ -26,30 +27,33 @@ def good_length(password):
     return -2
 
 
-def is_in_blacklist_file(password):
-    with open("blacklist.txt", "r") as file:
-        for line in file:
-            if password in line:
-                return -3
+def is_in_blacklist_file(password, blacklisted_words):
+    if password in blacklisted_words:
+        return -3
     return 3
 
 
+def get_blacklisted_words():
+    if not os.path.exists("blacklist.txt"):
+        return None
+    with open("blacklist.txt", "r") as file:
+        return (file.read()).split("\n")
+
+
 def get_password_strength(password):
-    strength = 0
-    strength += has_upper_and_lower_case(password)
-    strength += has_numeric_digits(password)
-    strength += has_special_char(password)
-    strength += good_length(password)
-    try:
-        strength += is_in_blacklist_file(password)
-    except FileNotFoundError:
-        pass
+    strength = sum([has_upper_and_lower_case(password),
+                    has_numeric_digits(password),
+                    has_special_char(password),
+                    good_length(password)])
+
+    blacklisted_words = get_blacklisted_words()
+    if blacklisted_words:
+        strength += is_in_blacklist_file(password, blacklisted_words)
     if strength < 0:
         return 0
     return strength
 
 
 if __name__ == '__main__':
-    password = input("Input password:")
-    password_strength = get_password_strength(password)
-    print("Password strength = ", password_strength)
+    password = getpass.getpass(prompt="Enter your password: ")
+    print("Password strength = ", get_password_strength(password))
